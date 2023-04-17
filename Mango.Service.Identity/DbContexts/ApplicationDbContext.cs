d6cd5f99a94fe4c86438b9db6a56045b58364a31
RinorGllareva <rg51523@ubt-uni.net>
2023-04-17 02:16:40 +0200

    Section 1 - QuickStart IdentityServer

---
+++ Mango.Service.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Mobox.Service.Identity.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mobox.Services.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        
+
+    }
+}
---
+++ SongAPI/Migrations/20230416155700_ConfigureDefaultIdentityTable.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SongAPI.Migrations
+{
+    public partial class ConfigureDefaultIdentityTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SongGenres");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SongGenres",
+                columns: table => new
+                {
+                    SongGenreId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    SongId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SongGenres", x => x.SongGenreId);
+                    table.ForeignKey(
+                        name: "FK_SongGenres_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "GenreId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_SongGenres_Songs_SongId",
+                        column: x => x.SongId,
+                        principalTable: "Songs",
+                        principalColumn: "SongId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SongGenres_GenreId",
+                table: "SongGenres",
+                column: "GenreId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SongGenres_SongId",
+                table: "SongGenres",
+                column: "SongId");
+        }
+    }
+}
